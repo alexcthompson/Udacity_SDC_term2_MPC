4462015d9f7de87c4eec97f1cc88490dcc4114d8
Alex Thompson <alexcthompson@gmail.com>
2018-08-12 20:35:58 -0700

    basic code 99% done, struggling with segfault

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 10;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -21,19 +21,109 @@ double dt = 0;
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
+double ref_v = 50;
+
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
 class FG_eval {
  public:
-  // Fitted polynomial coefficients
-  Eigen::VectorXd coeffs;
-  FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
+    // Fitted polynomial coefficients
+    Eigen::VectorXd coeffs;
+    FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
 
-  typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
-  void operator()(ADvector& fg, const ADvector& vars) {
-    // TODO: implement MPC
-    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
-    // NOTE: You'll probably go back and forth between this function and
-    // the Solver function below.
-  }
+    typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+    void operator()(ADvector& fg, const ADvector& vars) {
+        // cost in the first element of fg
+        fg[0] = 0.0;
+
+        // loop over states to add up cost
+        for (int step = 0; step < N; step++) {
+            // cte
+            fg[0] += CppAD::pow(vars[cte_start + step], 2);
+            fg[0] += CppAD::pow(vars[epsi_start + step], 2);
+            fg[0] += CppAD::pow(vars[v_start + step] - ref_v, 2); // velocity error
+        }
+        cout << "checkpoint z3" << endl;
+
+        // miniize use of controls
+        for (int step = 0; step < N - 1; step++) {
+            fg[0] += CppAD::pow(vars[delta_start + step], 2);
+            fg[0] += CppAD::pow(vars[a_start + step], 2);
+        }
+
+        // minimize change in controls
+        for (int step = 0; step < N - 2; step++) {
+            fg[0] += CppAD::pow(vars[delta_start + step + 1] - vars[delta_start + step], 2);
+            fg[0] += CppAD::pow(vars[a_start     + step + 1] - vars[a_start     + step], 2);
+        }
+
+        cout << "checkpoint z1" << endl;
+
+        // setup initial constrains
+        fg[1 + x_start]     = vars[x_start];
+        fg[1 + y_start]     = vars[y_start];
+        fg[1 + psi_start]   = vars[psi_start];
+        fg[1 + v_start]     = vars[v_start];
+        fg[1 + cte_start]   = vars[cte_start];
+        fg[1 + epsi_start]  = vars[epsi_start];
+        // fg[1 + delta_start] = vars[delta_start];
+        // fg[1 + a_start]     = vars[a_start];
+
+        cout << "checkpoint z2" << endl;
+        // set up motion model
+        // for (int step = 1; step < N - 1; step++) {
+        for (int step = 1; step < N; step++) {
+            AD<double> x_cur     = vars[x_start + step];
+            AD<double> y_cur     = vars[y_start + step];
+            AD<double> psi_cur   = vars[psi_start + step];
+            AD<double> v_cur     = vars[v_start + step];
+            AD<double> cte_cur   = vars[cte_start + step];
+            AD<double> epsi_cur  = vars[epsi_start + step];
+            // actuators not needed for current calculation
+
+            AD<double> x_pre     = vars[x_start + step - 1];
+            AD<double> y_pre     = vars[y_start + step - 1];
+            AD<double> psi_pre   = vars[psi_start + step - 1];
+            AD<double> v_pre     = vars[v_start + step - 1];
+            AD<double> cte_pre   = vars[cte_start + step - 1];
+            AD<double> epsi_pre  = vars[epsi_start + step - 1];
+            // do constrain actuators
+            AD<double> delta_pre = vars[delta_start + step - 1];
+            AD<double> a_pre     = vars[a_start + step - 1];
+
+            // other
+            AD<double> f_pre     = coeffs[0] + coeffs[1] * x_pre + coeffs[2] * CppAD::pow(x_pre, 2)
+                                 + coeffs[3] * CppAD::pow(x_pre, 3);
+            AD<double> psi_des_pre = CppAD::atan(coeffs[1] + coeffs[2] * x_pre +
+                                          coeffs[3] * CppAD::pow(x_pre, 2));
+
+            // now the constraints
+            // x, y, psi, v
+            fg[1 + x_start + step]    = x_cur -
+                                        (x_pre + v_pre * dt * CppAD::cos(psi_pre));
+            fg[1 + y_start + step]    = y_cur -
+                                        (y_pre + v_pre * dt * CppAD::sin(psi_pre));
+
+            // check this
+            fg[1 + psi_start + step]  = psi_cur -
+                                        (psi_pre - v_pre * dt * delta_pre / Lf);
+            // what's going on here?
+
+            fg[1 + v_start + step]    = v_cur -
+                                        (v_pre + a_pre  * dt);
+            fg[1 + cte_start + step]  = cte_cur -
+                                        ((f_pre - y_pre) + v_pre * dt * CppAD::sin(epsi_pre));
+            fg[1 + epsi_start + step] = epsi_cur -
+                                        ((psi_pre - psi_des_pre) - v_pre * delta_pre * dt / Lf);
+        }
+    }
 };
 
 //
@@ -43,79 +133,166 @@ MPC::MPC() {}
 MPC::~MPC() {}
 
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
-  bool ok = true;
-  size_t i;
-  typedef CPPAD_TESTVECTOR(double) Dvector;
+    bool ok = true;
+    typedef CPPAD_TESTVECTOR(double) Dvector;
 
-  // TODO: Set the number of model variables (includes both states and inputs).
-  // For example: If the state is a 4 element vector, the actuators is a 2
-  // element vector and there are 10 timesteps. The number of variables is:
-  //
-  // 4 * 10 + 2 * 9
-  size_t n_vars = 0;
-  // TODO: Set the number of constraints
-  size_t n_constraints = 0;
+    // TODO: Set the number of model variables (includes both states and inputs).
+    // For example: If the state is a 4 element vector, the actuators is a 2
+    // element vector and there are 10 timesteps. The number of variables is:
+    //
+    // 4 * 10 + 2 * 9
+    size_t n_vars = 6 * N + 2 * (N - 1);
+    // TODO: Set the number of constraints
+    size_t n_constraints = 6 * N;
 
-  // Initial value of the independent variables.
-  // SHOULD BE 0 besides initial state.
-  Dvector vars(n_vars);
-  for (int i = 0; i < n_vars; i++) {
-    vars[i] = 0;
-  }
+    // Initial value of the independent variables.
+    // SHOULD BE 0 besides initial state.
+    Dvector vars(n_vars);
+    for (int i = 0; i < n_vars; i++) {
+        vars[i] = 0;
+    }
+    cout << "checkpoint 1.1" << endl;
 
-  Dvector vars_lowerbound(n_vars);
-  Dvector vars_upperbound(n_vars);
-  // TODO: Set lower and upper limits for variables.
+    double x    = state[0];
+    double y    = state[1];
+    double psi  = state[2];
+    double v    = state[3];
+    double cte  = state[4];
+    double epsi = state[5];
 
-  // Lower and upper limits for the constraints
-  // Should be 0 besides initial state.
-  Dvector constraints_lowerbound(n_constraints);
-  Dvector constraints_upperbound(n_constraints);
-  for (int i = 0; i < n_constraints; i++) {
-    constraints_lowerbound[i] = 0;
-    constraints_upperbound[i] = 0;
-  }
+    // TODO: Set lower and upper limits for variables.
+    Dvector vars_lowerbound(n_vars);
+    Dvector vars_upperbound(n_vars);
 
-  // object that computes objective and constraints
-  FG_eval fg_eval(coeffs);
+    // vars[x_start] = x;
+    // vars[y_start] = y;
+    // vars[psi_start] = psi;
+    // vars[v_start] = v;
+    // vars[cte_start] = cte;
+    // vars[epsi_start] = epsi;
 
-  //
-  // NOTE: You don't have to worry about these options
-  //
-  // options for IPOPT solver
-  std::string options;
-  // Uncomment this if you'd like more print information
-  options += "Integer print_level  0\n";
-  // NOTE: Setting sparse to true allows the solver to take advantage
-  // of sparse routines, this makes the computation MUCH FASTER. If you
-  // can uncomment 1 of these and see if it makes a difference or not but
-  // if you uncomment both the computation time should go up in orders of
-  // magnitude.
-  options += "Sparse  true        forward\n";
-  options += "Sparse  true        reverse\n";
-  // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
-  // Change this as you see fit.
-  options += "Numeric max_cpu_time          0.5\n";
+    // Set all non-actuators upper and lowerlimits
+    // to the max negative and positive values.
+    for (int i = 0; i < delta_start; i++) {
+        vars_lowerbound[i] = -1.0e19;
+        vars_upperbound[i] =  1.0e19;
+    }
 
-  // place to return solution
-  CppAD::ipopt::solve_result<Dvector> solution;
+    // The upper and lower limits of delta are set to -25 and 25
+    // degrees (values in radians).
+    // NOTE: Feel free to change this to something else.
+    for (int i = delta_start; i < a_start; i++) {
+        // cout << "setting delta min/max" << endl;
+        // cout << i << ": " << vars_lowerbound[i] << ", " << vars_upperbound[i] << endl;
+        vars_lowerbound[i] = -0.436332;
+        vars_upperbound[i] =  0.436332;
+        // cout << i << ": " << vars_lowerbound[i] << ", " << vars_upperbound[i] << endl;
 
-  // solve the problem
-  CppAD::ipopt::solve<Dvector, FG_eval>(
-      options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
-      constraints_upperbound, fg_eval, solution);
+    }
+    // cout << "checkpoint 1.2" << endl;
+    // cout << "delta_start = " << delta_start << endl;
 
-  // Check some of the solution values
-  ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+    // Acceleration/decceleration upper and lower limits.
+    // NOTE: Feel free to change this to something else.
+    for (int i = a_start; i < n_vars; i++) {
+        vars_lowerbound[i] = -1.0;
+        vars_upperbound[i] =  1.0;
+    }
 
-  // Cost
-  auto cost = solution.obj_value;
-  std::cout << "Cost " << cost << std::endl;
+    // Lower and upper limits for the constraints
+    // Should be 0 besides initial state.
+    Dvector constraints_lowerbound(n_constraints);
+    Dvector constraints_upperbound(n_constraints);
+    for (int i = 0; i < n_constraints; i++) {
+        constraints_lowerbound[i] = 0;
+        constraints_upperbound[i] = 0;
+    }
 
-  // TODO: Return the first actuator values. The variables can be accessed with
-  // `solution.x[i]`.
-  //
-  // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
-  // creates a 2 element double vector.
-  return {};
+    constraints_lowerbound[x_start] = x;
+    constraints_lowerbound[y_start] = y;
+    constraints_lowerbound[psi_start] = psi;
+    constraints_lowerbound[v_start] = v;
+    constraints_lowerbound[cte_start] = cte;
+    constraints_lowerbound[epsi_start] = epsi;
+
+    constraints_upperbound[x_start] = x;
+    constraints_upperbound[y_start] = y;
+    constraints_upperbound[psi_start] = psi;
+    constraints_upperbound[v_start] = v;
+    constraints_upperbound[cte_start] = cte;
+    constraints_upperbound[epsi_start] = epsi;
+
+    cout << "=== constraints ===" << endl;
+    for (int i = 0; i < n_constraints; i++) {
+        cout << i << ": " << constraints_lowerbound[i] << ", " << constraints_upperbound[i] << endl;
+    }
+
+    cout << "checkpoint 1.3" << endl;
+
+    // object that computes objective and constraints
+    FG_eval fg_eval(coeffs);
+    cout << "checkpoint 1.4" << endl;
+
+    //
+    // NOTE: You don't have to worry about these options
+    //
+    // options for IPOPT solver
+    std::string options;
+    // Uncomment this if you'd like more print information
+    options += "Integer print_level  0\n";
+    // NOTE: Setting sparse to true allows the solver to take advantage
+    // of sparse routines, this makes the computation MUCH FASTER. If you
+    // can uncomment 1 of these and see if it makes a difference or not but
+    // if you uncomment both the computation time should go up in orders of
+    // magnitude.
+    options += "Sparse  true        forward\n";
+    options += "Sparse  true        reverse\n";
+    // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+    // Change this as you see fit.
+    options += "Numeric max_cpu_time          2.0\n";
+
+    // place to return solution
+    CppAD::ipopt::solve_result<Dvector> solution;
+    cout << "checkpoint 1.5" << endl;
+
+    // Debugging
+    cout << "=== DEBUG ===" << endl;
+
+    cout << "vars.size() = " << vars.size() << endl;
+    cout << "vars_lowerbound = " << vars_lowerbound.size() << endl;
+    cout << "vars_upperbound = " << vars_upperbound.size() << endl;
+    cout << "constraints_lowerbound = " << constraints_lowerbound.size() << endl;
+    cout << "constraints_upperbound = " << constraints_upperbound.size() << endl;
+
+    // cout << "\ni: lower bound, var, upper bound" << endl;
+    // for (int i = 0; i < vars.size(); i++) {
+    //     cout << i << ": " << vars_lowerbound[i] << ", " << vars[i] << ", " << vars_upperbound[i] << endl;
+    // }
+    // solve the problem
+    CppAD::ipopt::solve<Dvector, FG_eval>(
+            options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
+            constraints_upperbound, fg_eval, solution);
+    cout << "checkpoint 1.51" << endl;
+
+    // Check some of the solution values
+    ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
+
+    // Cost
+    auto cost = solution.obj_value;
+    std::cout << "Cost " << cost << std::endl;
+    cout << "checkpoint 1.6" << endl;
+
+    // TODO: Return the first actuator values. The variables can be accessed with
+    // `solution.x[i]`.
+    vector<double> res;
+    res.push_back(solution.x[delta_start]);
+    res.push_back(solution.x[a_start]);
+    cout << "checkpoint 1.7" << endl;
+
+    for (int step = 0; step < N; step++) {
+        res.push_back(solution.x[x_start + step]);
+        res.push_back(solution.x[y_start + step]);
+    }
+
+    return res;
 }
